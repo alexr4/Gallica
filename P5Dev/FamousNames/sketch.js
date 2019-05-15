18699daab7e36abc87968eb350d7d99156951dbc
ALEX-SURFACE\ariva <alex@bonjour-lab.com>
2019-05-15 16:01:12 +0200

    no message

---
+++ P5Dev/FamousNames/sketch.js
@@ -0,0 +1,84 @@
+//original data
+var requestURL = '../../Datas/gallica_monographies.json';
+var datas;
+var records;
+
+//saved sorted data here
+var UISave;
+var exportRecords = [];
+var dictionary = [];
+
+function preload() {
+  //We retreive the DATA as a JSON File using the HTTPS Request
+  datas = loadJSON(requestURL);
+}
+
+function setup() {
+  records = datas.records;
+  console.log("based JSON data ", records);
+
+  UISave = createButton('saved new JSON');
+  UISave.mouseReleased(savedJSON);
+
+  let dateMin = 1600;
+  let dateMax = 1800;
+  for(let i=0; i<records.length; i++){
+    let value = (records[i].contributor != undefined) ? records[i].contributor : "undefined";
+    let date = (records[i].date != undefined) ? records[i].date : "undefined";
+    date = parseDate(date);
+    if(date.length > 4){
+      date = date.substring(0, 4);
+    }
+    date = Number(date);
+
+    if(date >= dateMin && date <= dateMax){
+      //The following lines help to clean contributors
+      //value = (value === "undefined") ? value : "authors"; //test how many author are known
+      value = cleanLastIterationAt(value, '(', 3); //remove all elements after the last ( which can be birthdate, job, place...
+      value = cleanLastIterationAt(value, '.', 3); //remove all elements after the last . which can be birthdate, job, place...
+      value = cleanLastIterationAt(value, '\ʿ', 3); //remove ʿ at the begining of an arabic names
+      value = cleanFirstIterationAt(value, '\ʿ', 3);//remove ʿ at the end of an arabic names
+      value = cleanFirstIterationAt(value, ' ', 3);//remove ' ' at begining of the name
+      value = value.replace(',', ""); //remove remaining , betwen surename and name
+      if(value.indexOf("Madame") > -1 || value.indexOf("Monsier") > -1 || value.split(" ").length < 2){
+        value = "undefined";
+      }
+
+      //find a match inside known names
+      for(let j=0; j<famousName.length; j++){
+        let nameToTest = famousName[j];
+        let normalizeTest = nameToTest.toLowerCase();
+        let normalizeName = value.toLowerCase();
+        let ressemblanceRatio = similarity(normalizeTest, normalizeName);
+        if(ressemblanceRatio > 0.5){
+          //console.log(ressemblanceRatio, normalizeTest, normalizeName)
+          count2DPer(date, nameToTest, dictionary);
+          break;
+        }
+      }
+    }
+    if(i%1000 == 0) console.log("process...")
+  }
+
+  //console.log(sorted1DDictionary(dictionary));
+
+  for(key in dictionary){
+    let subdict = dictionary[key]
+    console.log(key);
+    for(subkey in subdict){
+      console.log("\t", subkey, subdict[subkey])
+    }
+  }
+
+  console.log("array has been sorted");
+  exportRecords = convertDictionaryIntoJSONArray(dictionary);
+  console.log("Dictionary to JSON ready to export: ");
+}
+
+
+function savedJSON(){
+  console.log("save data");
+  let json = {};
+  json.records = exportRecords;
+  saveJSON(json, 'sortedData.json');
+}
