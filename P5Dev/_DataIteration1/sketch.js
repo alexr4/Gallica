ef8578f364ebbb395fffd0c26d84f5eba0bd3bc5
ALEX-SURFACE\ariva <alex@bonjour-lab.com>
2019-05-16 14:42:21 +0200

    no message

--- P5Dev/_DataIteration1/sketch.js
+++ P5Dev/_DataIteration1/sketch.js
@@ -21,27 +21,36 @@ function draw(){
   let max = 70;
   let min = 1;
   let innerRadius = 200;
-  let outerRadius = 400;
+  let lenMax = 200;
+  let res = 25;
+  colorMode(HSB, 1, 1, 1)
+
+
+
   for(let i=0; i<nameArray.length; i++){
     let name = nameArray[i];
     let value = nameJSONOBject[name];
 
     let angle = map(i, 0, nameArray.length, 0, TWO_PI);
-    let len = map(value, 0, 70, innerRadius, outerRadius);
+    let len = map(value, 0, 100, 0, lenMax);
+    let lenH = map(value, 0, 100, res * .1, res);
+    let hue = value / 100;
     let px = cos(angle) * innerRadius + width/2;
     let py = sin(angle) * innerRadius + height/2;
 
     push();
     translate(width/2, height/2);
     rotate(angle);
-    stroke(255);
-    line(innerRadius, 0, len, 0);
+    noStroke();
+    fill(hue, 1, 1);
+    rect(innerRadius, lenH * -0.5, len, lenH);
     pop();
 
     push();
     translate(px, py);
     rotate(angle);
-    fill(255);
+    fill(0, 0, 1);
+    noStroke();
     textAlign(RIGHT, CENTER)
     textSize(6)
     text(name, -10, 0)
@@ -49,6 +58,9 @@ function draw(){
 
   }
 
-  save("hello.svg");
-  noLoop();
+
+
+  let frame = nf(frameCount, 5);
+  //save("datavis_"+frame+".svg");
+  //noLoop();
 }
